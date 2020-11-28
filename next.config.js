839c6b51155b7ef23e196a5ef37a993bc068bb8f
jorge abascal lopez <jorgeabascal20@gmail.com>
2020-11-28 14:54:45 -0500

    Agregando o habilitando cors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,37 @@
 module.exports = {
     env: {
         backendURL: "https://enigmatic-everglades-41199.herokuapp.com/",
-        frontendURL: "https://fireclone-agbgstjl2.vercel.app/"
-    }
+        frontendURL: "https://firesend.vercel.app/"
+    },
+    async headers() {
+        return [
+          {
+            // matching all API routes
+            source: "/api/:path*",
+            headers: [
+              { key: "Access-Control-Allow-Credentials", value: "true" },
+              { key: "Access-Control-Allow-Origin", value: "*" },
+              { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+              { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+            ]
+          }
+        ]
+      }
 }
+
+/*module.exports = {
+    async headers() {
+      return [
+        {
+          // matching all API routes
+          source: "/api/:path*",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
+        }
+      ]
+    }
+  };
